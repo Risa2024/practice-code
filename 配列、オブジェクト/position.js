f68a5650758e3922164e074a4cff7a3c921626fc
Risa Goto <omusubi5656dup@gmail.com>
2024-11-09 17:04:34 +0900

    add position & object

---
+++ 配列、オブジェクト/position.js
@@ -0,0 +1,14 @@
+'use strict';
+//宣言、初期化
+//let colors = ['red','Green', 'Blue'];
+//console.log(colors);
+//console.log(colors[0]);
+//console.log(colors[1]);
+//console.log(colors[2]);
+//console.log(colors[3]);
+
+//データ型
+//let arr = [1, '2', 3, ['a', 'b', 'c']];
+//console.log(arr);
+
+let colors = []
