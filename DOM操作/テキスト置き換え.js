19af762d9135a3bac6db9aa025f2b81370b033e6
Risa Goto <omusubi5656dup@gmail.com>
2024-11-10 19:16:06 +0900

    add DOM

--- DOM操作/テキスト置き換え.js
+++ DOM操作/テキスト置き換え.js
@@ -1,12 +1,8 @@
 'use strict';
-
 let btn = document.getElementById('triggerButton');
 btn.addEventListener('click', function(e){
-  // alert('clicked!');
   // IDによるノードの取得
   let headerTitle = document.getElementById('headerTitle');
-  // let headerTitle = document.getElementById('hoge');
-  console.log(headerTitle);
-  // コンテンツの取得（テキストの取得）
-  console.log(headerTitle.textContent);
+  // テキストの置換
+  headerTitle.textContent = 'タイトルが変更されました';
 }, false);
